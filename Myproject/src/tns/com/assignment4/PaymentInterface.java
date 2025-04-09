c901886778234481914d4c4efd632420b88c3b5d
raksh <raksh@PC1>
2025-04-09 11:42:12 +0530

    Assignment4

---
+++ Myproject/src/tns/com/assignment4/PaymentInterface.java
@@ -0,0 +1,11 @@
+package tns.com.assignment4;
+ interface PaymentInterface {
+		 void process(double amt);
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+
+		}
+
+	}
+
